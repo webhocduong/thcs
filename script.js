f8a411f3d4541caa6accfefb6e750051a135f4f3
webhocduong <ioe2thcshc@gmail.com>
2026-04-19 14:49:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-const contactBtn = document.getElementById("contactBtn");
+import { auth } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-if (contactBtn) {
-  contactBtn.addEventListener("click", () => {
-    alert("Cảm ơn bạn đã ghé thăm! Bạn có thể liên hệ qua email: ban@example.com");
-  });
-}
+// ĐĂNG KÝ
+document.getElementById("registerBtn").addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert("Đăng ký thành công!");
+  } catch (error) {
+    alert(error.message);
+  }
+});
+
+// ĐĂNG NHẬP
+document.getElementById("loginBtn").addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("Đăng nhập thành công!");
+  } catch (error) {
+    alert(error.message);
+  }
+});
