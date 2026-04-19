d78ee9f98e24a01b6dd7a378b89c6372abbf04f0
webhocduong <ioe2thcshc@gmail.com>
2026-04-19 14:24:12 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,7 +3,27 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 
 // Cấu hình Firebase (THAY bằng config của bạn)
 const firebaseConfig = {
-  apiKey: "DÁN_API_KEY",
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDefd3yBo4rhceclxAHKeykx6YbjjAwlJw",
+  authDomain: "thcs-77d87.firebaseapp.com",
+  projectId: "thcs-77d87",
+  storageBucket: "thcs-77d87.firebasestorage.app",
+  messagingSenderId: "98344833709",
+  appId: "1:98344833709:web:4ae3e87f3ea0befb552c24",
+  measurementId: "G-79TEGRBEKC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
   authDomain: "DÁN_AUTH_DOMAIN",
   projectId: "DÁN_PROJECT_ID",
   storageBucket: "DÁN_STORAGE_BUCKET",
