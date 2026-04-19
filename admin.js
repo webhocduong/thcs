a97c84bb7c66bc4de81444eb52bcdcfd3635b323
webhocduong <ioe2thcshc@gmail.com>
2026-04-19 17:19:35 +0700

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,21 @@
+import { auth } from "./firebase.js";
+import { signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// 👉 kiểm tra nếu không phải admin thì đá ra ngoài
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    if (user.email !== "admin@gmail.com") {
+      alert("Bạn không phải admin!");
+      window.location.href = "index.html";
+    }
+  } else {
+    window.location.href = "index.html";
+  }
+});
+
+// 👉 đăng xuất
+document.getElementById("logoutBtn").addEventListener("click", async () => {
+  await signOut(auth);
+  alert("Đã đăng xuất");
+  window.location.href = "index.html";
+});
