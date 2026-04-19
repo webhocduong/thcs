767f63d35520c4a8fb34e0b590ceecaac7dc55f2
webhocduong <ioe2thcshc@gmail.com>
2026-04-19 16:26:10 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,2 +1,11 @@
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDefd3yBo4rhceclxAHKeykx6YbjjAwlJw",
+  authDomain: "thcs-77d87.firebaseapp.com",
+  projectId: "thcs-77d87",
+  storageBucket: "thcs-77d87.firebasestorage.app",
+  messagingSenderId: "98344833709",
+  appId: "1:98344833709:web:4543a92e785203a1552c24",
+  measurementId: "G-KF9RFE2NJ2"
+};
 
