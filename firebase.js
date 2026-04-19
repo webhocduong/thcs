13d3b87f4954cd7f29e7713a9759a414838c9369
webhocduong <ioe2thcshc@gmail.com>
2026-04-19 16:34:46 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,15 +5,14 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 
 // Cấu hình Firebase (của bạn)
 const firebaseConfig = {
-  apiKey: "AIzaSyDefd3yBo4hceclxAHKeykx6YbjjAwIJw",
+  apiKey: "AIzaSyDefd3yBo4rhceclxAHKeykx6YbjjAwlJw",
   authDomain: "thcs-77d87.firebaseapp.com",
   projectId: "thcs-77d87",
-  storageBucket: "thcs-77d87.appspot.com", 
+  storageBucket: "thcs-77d87.firebasestorage.app",
   messagingSenderId: "98344833709",
   appId: "1:98344833709:web:4543a92e785203a1552c24",
   measurementId: "G-KF9RFE2NJ2"
 };
-
 
 // Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
