492a32f31bf5a4aa2d759de600d3af4e0785f38b
webhocduong <ioe2thcshc@gmail.com>
2026-04-19 14:49:04 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,22 @@
 // Import Firebase từ CDN
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// Cấu hình Firebase
+// Config Firebase của bạn
 const firebaseConfig = {
-  apiKey: "AIzaSyDefd3yBo4hceclxAHKeykx6YbjJAwiJw",
+  apiKey: "AIzaSyDefd3yBo4hceclxAHKeykx6YbjJAwIjw",
   authDomain: "thcs-77d87.firebaseapp.com",
   projectId: "thcs-77d87",
-  storageBucket: "thcs-77d87.firebasestorage.app",
+  storageBucket: "thcs-77d87.appspot.com",
   messagingSenderId: "98344833709",
-  appId: "1:98344833709:web:4ae3e87f3ea0befb552c24",
-  measurementId: "G-79TEGRBEKC"
+  appId: "1:98344833709:web:4ae3e87f3ea0befb552c24"
 };
 
 // Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export
-export { app };
+// Khởi tạo Auth
+const auth = getAuth(app);
+
+// Export ra để dùng file khác
+export { auth };
