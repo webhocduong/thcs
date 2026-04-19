4a657e3a587220d906b33e55a8c42eaa6e5c5783
webhocduong <ioe2thcshc@gmail.com>
2026-04-19 16:17:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-// Import Firebase
-import { auth } from "./firebase.js";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } 
+// Import Firebase trực tiếp
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// Config Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDefd3yBo4hceclxAHKeykx6YbjjAwIJw",
+  authDomain: "thcs-77d87.firebaseapp.com",
+  projectId: "thcs-77d87",
+  storageBucket: "thcs-77d87.firebasestorage.app",
+  messagingSenderId: "98344833709",
+  appId: "1:98344833709:web:4543a92e785203a1552c24"
+};
+
+// Khởi tạo
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 // Lấy nút
 const registerBtn = document.getElementById("registerBtn");
@@ -15,6 +29,19 @@ registerBtn.addEventListener("click", async () => {
   try {
     await createUserWithEmailAndPassword(auth, email, password);
     alert("Đăng ký thành công!");
+  } catch (error) {
+    alert(error.message);
+  }
+});
+
+// Đăng nhập
+loginBtn.addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("Đăng nhập thành công!");
   } catch (error) {
     alert(error.message);
   }
