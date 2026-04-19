608e6eea47afa5ac0b41b14eff05ea42f82714a1
webhocduong <ioe2thcshc@gmail.com>
2026-04-19 14:29:10 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,9 @@
-// Import Firebase
+// Import Firebase từ CDN (quan trọng)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-// Cấu hình Firebase (THAY bằng config của bạn)
+// Cấu hình Firebase của bạn
 const firebaseConfig = {
-  // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDefd3yBo4rhceclxAHKeykx6YbjjAwlJw",
+  apiKey: "AIzaSyDefd3yBo4hceclxAHKeykx6YbjJAwiJw",
   authDomain: "thcs-77d87.firebaseapp.com",
   projectId: "thcs-77d87",
   storageBucket: "thcs-77d87.firebasestorage.app",
@@ -21,17 +12,8 @@ const firebaseConfig = {
   measurementId: "G-79TEGRBEKC"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-  authDomain: "DÁN_AUTH_DOMAIN",
-  projectId: "DÁN_PROJECT_ID",
-  storageBucket: "DÁN_STORAGE_BUCKET",
-  messagingSenderId: "DÁN_MESSAGING_ID",
-  appId: "DÁN_APP_ID"
-};
-
 // Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
 
+// Export để dùng ở file khác
 export { app };
