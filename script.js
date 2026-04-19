434da46b79759fa9f62b83a04862a43a7683e0da
webhocduong <ioe2thcshc@gmail.com>
2026-04-19 15:24:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 // Import Firebase
 import { auth } from "./firebase.js";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } 
