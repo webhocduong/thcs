97d08f78b5577b5a8cb215d195f067dbdeebbac0
webhocduong <ioe2thcshc@gmail.com>
2026-04-19 15:40:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,32 +2,33 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } 
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// Import file firebase của bạn
+// Import firebase config
 import { auth } from "./firebase.js";
 
-// Đợi web load xong
 document.addEventListener("DOMContentLoaded", () => {
 
-    // Lấy nút
     const registerBtn = document.getElementById("registerBtn");
     const loginBtn = document.getElementById("loginBtn");
 
-    // ===== ĐĂNG KÝ =====
+    console.log("JS đã chạy"); // test
+
+    // ĐĂNG KÝ
     registerBtn.addEventListener("click", async () => {
+        console.log("Đã bấm đăng ký"); // test
+
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
 
         try {
             const userCredential = await createUserWithEmailAndPassword(auth, email, password);
             alert("Đăng ký thành công!");
-            console.log(userCredential.user);
         } catch (error) {
             alert(error.message);
             console.error(error);
         }
     });
 
-    // ===== ĐĂNG NHẬP =====
+    // ĐĂNG NHẬP
     loginBtn.addEventListener("click", async () => {
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
@@ -35,10 +36,8 @@ document.addEventListener("DOMContentLoaded", () => {
         try {
             const userCredential = await signInWithEmailAndPassword(auth, email, password);
             alert("Đăng nhập thành công!");
-            console.log(userCredential.user);
         } catch (error) {
             alert(error.message);
-            console.error(error);
         }
     });
 
