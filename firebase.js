70757c44441c643b4735a0fa046f019d317a767e
webhocduong <ioe2thcshc@gmail.com>
2026-04-26 14:46:19 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
-// Import Firebase
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
- const firebaseConfig = {
-    const firebaseConfig = {
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+  const firebaseConfig = {
     apiKey: "AIzaSyDefd3yBo4rhceclxAHKeykx6YbjjAwlJw",
     authDomain: "thcs-77d87.firebaseapp.com",
     projectId: "thcs-77d87",
@@ -11,9 +11,6 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
     appId: "1:98344833709:web:4543a92e785203a1552c24",
     measurementId: "G-KF9RFE2NJ2"
   };
-// Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-// Export để dùng ở file khác
-export { auth };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
