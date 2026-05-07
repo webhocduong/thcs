b730f1fd86d0b57e98a1c8cf8e6e581715078f98
webhocduong <ioe2thcshc@gmail.com>
2026-05-07 18:13:51 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 // FIREBASE CONFIG
 const firebaseConfig = {
-    apiKey: "AIzaSyDefd3yBo4rheeclxAHkeykx6YbjjAwlJw",
-    authDomain: "thcs-77d87.firebaseapp.com",
-    projectId: "thcs-77d87",
-    storageBucket: "thcs-77d87.firebasestorage.app",
-    messagingSenderId: "98344833709",
-    appId: "1:98344833709:web:4543a92e785203a1552c24"
-    measurementId: "G-KF9RFE2NJ2"
+  apiKey: "AIzaSyDefd3yBo4rhceclxAHKeykx6YbjjAwlJw",
+  authDomain: "thcs-77d87.firebaseapp.com",
+  projectId: "thcs-77d87",
+  storageBucket: "thcs-77d87.firebasestorage.app",
+  messagingSenderId: "98344833709",
+  appId: "1:98344833709:web:4543a92e785203a1552c24",
+  measurementId: "G-KF9RFE2NJ2"
 };
 // KHỞI TẠO FIREBASE
 const app = initializeApp(firebaseConfig);
