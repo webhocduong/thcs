304ab4adb38668e432519a8acf2d18fa6a5d565b
webhocduong <ioe2thcshc@gmail.com>
2026-05-07 17:56:37 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,32 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+// FIREBASE CONFIG
 const firebaseConfig = {
-  apiKey: "AIzaSyDefd3yBo4rhceclxAHKeykx6YbjjAwlJw",
-  authDomain: "thcs-77d87.firebaseapp.com",
-  projectId: "thcs-77d87",
-  storageBucket: "thcs-77d87.firebasestorage.app",
-  messagingSenderId: "98344833709",
-  appId: "1:98344833709:web:4543a92e785203a1552c24",
-  measurementId: "G-KF9RFE2NJ2"
+
+    apiKey: "AIzaSyDefd3yBo4rheeclxAHkeykx6YbjjAwlJw",
+
+    authDomain: "thcs-77d87.firebaseapp.com",
+
+    projectId: "thcs-77d87",
+
+    storageBucket: "thcs-77d87.appspot.com",
+
+    messagingSenderId: "98344833709",
+
+    appId: "1:98344833709:web:4543a92e785203a1552c24"
 };
-// Khởi tạo
+
+
+// KHỞI TẠO FIREBASE
 const app = initializeApp(firebaseConfig);
-// Export
+
+
+// EXPORT
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
