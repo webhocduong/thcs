ca77698ea3456a211c326c655b6b981eebdff296
webhocduong <ioe2thcshc@gmail.com>
2026-05-07 18:03:33 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,32 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-
 // FIREBASE CONFIG
 const firebaseConfig = {
-
     apiKey: "AIzaSyDefd3yBo4rheeclxAHkeykx6YbjjAwlJw",
-
     authDomain: "thcs-77d87.firebaseapp.com",
-
     projectId: "thcs-77d87",
-
     storageBucket: "thcs-77d87.appspot.com",
-
     messagingSenderId: "98344833709",
-
     appId: "1:98344833709:web:4543a92e785203a1552c24"
 };
-
-
 // KHỞI TẠO FIREBASE
 const app = initializeApp(firebaseConfig);
-
-
 // EXPORT
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
