c1017e4f4881dea36d1e4872cd4e02de3f1fb659
webhocduong <ioe2thcshc@gmail.com>
2026-05-07 18:08:42 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDefd3yBo4rheeclxAHkeykx6YbjjAwlJw",
     authDomain: "thcs-77d87.firebaseapp.com",
     projectId: "thcs-77d87",
-    storageBucket: "thcs-77d87.appspot.com",
+    storageBucket: "thcs-77d87.firebasestorage.app",
     messagingSenderId: "98344833709",
     appId: "1:98344833709:web:4543a92e785203a1552c24"
 };
