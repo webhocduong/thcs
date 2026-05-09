ecaac2d3141c4572815126b23e2efdd3e37fc6a5
webhocduong <ioe2thcshc@gmail.com>
2026-05-09 11:01:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
-import {
-  auth
-}
-from "./firebase.js";
+import { auth } from "./firebase.js";
 
 import {
 
-  createUserWithEmailAndPassword,
+    createUserWithEmailAndPassword,
 
-  signInWithEmailAndPassword,
+    signInWithEmailAndPassword,
 
-  signOut,
+    onAuthStateChanged,
 
-  onAuthStateChanged
+    signOut
 
-}
-from
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-/* INPUT */
-
-const emailInput =
+const email =
 document.getElementById("email");
 
-const passwordInput =
+const password =
 document.getElementById("password");
-
-/* BUTTON */
 
 const registerBtn =
 document.getElementById("registerBtn");
@@ -33,154 +24,95 @@ document.getElementById("registerBtn");
 const loginBtn =
 document.getElementById("loginBtn");
 
-const logoutBtn =
-document.getElementById("logoutBtn");
+const guestButtons =
+document.getElementById("guestButtons");
 
-const userBtn =
-document.getElementById("userBtn");
-
-/* SECTION */
-
-const authSection =
-document.getElementById("authSection");
-
-const userSection =
-document.getElementById("userSection");
+const userMenu =
+document.getElementById("userMenu");
 
 const userEmail =
 document.getElementById("userEmail");
 
-const dropdownMenu =
-document.getElementById("dropdownMenu");
+const logoutBtn =
+document.getElementById("logoutBtn");
 
-const result =
-document.getElementById("result");
 
-/* REGISTER */
+// ĐĂNG KÝ
 
-registerBtn.addEventListener(
-"click",
-async()=>{
+registerBtn.onclick = async () => {
 
-  try{
+    try{
 
-    await
-    createUserWithEmailAndPassword(
+        await createUserWithEmailAndPassword(
 
-      auth,
+            auth,
 
-      emailInput.value,
+            email.value,
 
-      passwordInput.value
+            password.value
 
-    );
+        );
 
-    result.innerText =
-    "Đăng ký thành công";
+        alert("Đăng ký thành công");
 
-  }
+    }catch(error){
 
-  catch(error){
+        alert(error.message);
+    }
+};
 
-    result.innerText =
-    error.message;
-  }
 
+// ĐĂNG NHẬP
+
+loginBtn.onclick = async () => {
+
+    try{
+
+        await signInWithEmailAndPassword(
+
+            auth,
+
+            email.value,
+
+            password.value
+
+        );
+
+        alert("Đăng nhập thành công");
+
+    }catch(error){
+
+        alert(error.message);
+    }
+};
+
+
+// KIỂM TRA LOGIN
+
+onAuthStateChanged(auth, (user)=>{
+
+    if(user){
+
+        guestButtons.style.display = "none";
+
+        userMenu.style.display = "flex";
+
+        userEmail.innerText = user.email;
+
+    }else{
+
+        guestButtons.style.display = "flex";
+
+        userMenu.style.display = "none";
+    }
 });
 
-/* LOGIN */
 
-loginBtn.addEventListener(
-"click",
-async()=>{
+// ĐĂNG XUẤT
 
-  try{
+logoutBtn.onclick = async ()=>{
 
-    await
-    signInWithEmailAndPassword(
+    await signOut(auth);
 
-      auth,
-
-      emailInput.value,
-
-      passwordInput.value
-
-    );
-
-    result.innerText =
-    "Đăng nhập thành công";
-
-  }
-
-  catch(error){
-
-    result.innerText =
-    error.message;
-  }
-
-});
-
-/* LOGOUT */
-
-logoutBtn.addEventListener(
-"click",
-async()=>{
-
-  await signOut(auth);
-
-});
-
-/* CHECK LOGIN */
-
-onAuthStateChanged(
-auth,
-(user)=>{
-
-  if(user){
-
-    authSection.style.display =
-    "none";
-
-    userSection.style.display =
-    "block";
-
-    userEmail.innerText =
-    user.email;
-
-  }
-
-  else{
-
-    authSection.style.display =
-    "flex";
-
-    userSection.style.display =
-    "none";
-  }
-
-});
-
-/* MENU */
-
-userBtn.addEventListener(
-"click",
-()=>{
-
-  if(
-    dropdownMenu.style.display
-    ===
-    "block"
-  ){
-
-    dropdownMenu.style.display =
-    "none";
-
-  }
-
-  else{
-
-    dropdownMenu.style.display =
-    "block";
-  }
-
-});
+    location.reload();
+};
