b98d035fb75fadd15565e42514976685ac115b15
webhocduong <ioe2thcshc@gmail.com>
2026-05-09 20:35:12 +0700

    Update post.js

--- post.js
+++ post.js
@@ -7,115 +7,147 @@ import {
 
     collection,
 
-    getDocs,
+    addDoc,
 
-    deleteDoc,
-
-    doc
+    serverTimestamp
 
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+import {
 
-const postsContainer =
-document.getElementById("postsContainer");
+    onAuthStateChanged
 
-
-async function loadPosts(){
-
-    const querySnapshot =
-    await getDocs(collection(db, "posts"));
-
-    postsContainer.innerHTML = "";
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 
-    querySnapshot.forEach((docSnap)=>{
+const title =
+document.getElementById("title");
 
-        const post =
-        docSnap.data();
+const content =
+document.getElementById("content");
 
-        const div =
-        document.createElement("div");
+const imageInput =
+document.getElementById("imageInput");
 
-        div.className = "postCard";
+const postBtn =
+document.getElementById("postBtn");
 
-
-        div.innerHTML = `
-
-            ${
-                post.imageUrl
-
-                ?
-
-                `<img
-                    src="${post.imageUrl}"
-                    class="postImage"
-                >`
-
-                :
-
-                ""
-            }
-
-            <h2>
-                ${post.title}
-            </h2>
-
-            <p>
-                ${post.content}
-            </p>
-
-            <small>
-                Đăng bởi:
-                ${post.userEmail}
-            </small>
-
-            <br><br>
-
-            ${
-                auth.currentUser &&
-                auth.currentUser.uid === post.userId
-
-                ?
-
-                `<button
-                    class="deleteBtn"
-                    data-id="${docSnap.id}">
-                    Xóa bài
-                </button>`
-
-                :
-
-                ""
-            }
-
-        `;
+let currentUser = null;
 
 
-        postsContainer.appendChild(div);
-    });
+/* CHECK LOGIN */
+
+onAuthStateChanged(auth, (user)=>{
+
+    if(user){
+
+        currentUser = user;
+
+    }else{
+
+        alert("Bạn phải đăng nhập");
+
+        window.location.href = "index.html";
+    }
+});
 
 
-    /* XÓA BÀI */
+/* ĐĂNG BÀI */
 
-    document
-    .querySelectorAll(".deleteBtn")
+postBtn.addEventListener("click", async ()=>{
 
-    .forEach((btn)=>{
+    if(!currentUser){
 
-        btn.onclick = async ()=>{
+        alert("Chưa đăng nhập");
 
-            await deleteDoc(
+        return;
+    }
 
-                doc(
-                    db,
-                    "posts",
-                    btn.dataset.id
-                )
+    if(title.value.trim() === ""){
+
+        alert("Nhập tiêu đề");
+
+        return;
+    }
+
+    if(content.value.trim() === ""){
+
+        alert("Nhập nội dung");
+
+        return;
+    }
+
+    try{
+
+        let imageUrl = "";
+
+
+        /* UPLOAD ẢNH */
+
+        if(imageInput.files[0]){
+
+            const file =
+            imageInput.files[0];
+
+            const formData =
+            new FormData();
+
+            formData.append("file", file);
+
+            formData.append(
+                "upload_preset",
+                "school_upload"
             );
 
-            loadPosts();
-        };
-    });
-}
 
-loadPosts();
+            const response =
+            await fetch(
+
+                "https://api.cloudinary.com/v1_1/dfoo4jpkz/image/upload",
+
+                {
+                    method:"POST",
+                    body:formData
+                }
+            );
+
+            const data =
+            await response.json();
+
+            imageUrl =
+            data.secure_url;
+        }
+
+
+        /* LƯU FIREBASE */
+
+        await addDoc(
+
+            collection(db, "posts"),
+
+            {
+
+                title: title.value,
+
+                content: content.value,
+
+                imageUrl: imageUrl,
+
+                userEmail: currentUser.email,
+
+                userId: currentUser.uid,
+
+                createdAt: serverTimestamp()
+
+            }
+        );
+
+        alert("Đăng bài thành công");
+
+        window.location.href = "posts.html";
+
+    }catch(error){
+
+        alert(error.message);
+    }
+});
