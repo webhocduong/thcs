da44a09506c69565f2a0039edcdd64194c90c904
webhocduong <ioe2thcshc@gmail.com>
2026-05-16 17:38:48 +0700

    Update post.js

--- post.js
+++ post.js
@@ -232,8 +232,124 @@ urlParams.get("myposts");
 
 /* POST */
 
-postBtn.onclick = async ()=>{
+postBtn.onclick = async () => {
 
+    if (!currentUser) {
+
+        alert("Bạn chưa đăng nhập");
+
+        return;
+    }
+
+    if (title.value.trim() === "") {
+
+        alert("Nhập tiêu đề");
+
+        return;
+    }
+
+    if (content.value.trim() === "") {
+
+        alert("Nhập nội dung");
+
+        return;
+    }
+
+    try {
+
+        let imageUrl = "";
+
+
+        /* UPLOAD ẢNH */
+
+        if (
+            imageInput &&
+            imageInput.files &&
+            imageInput.files.length > 0
+        ) {
+
+            const file =
+            imageInput.files[0];
+
+            const formData =
+            new FormData();
+
+            formData.append(
+                "file",
+                file
+            );
+
+            formData.append(
+                "upload_preset",
+                "school_upload"
+            );
+
+            const response =
+            await fetch(
+                "https://api.cloudinary.com/v1_1/dfoo4jpkz/image/upload",
+                {
+                    method: "POST",
+                    body: formData
+                }
+            );
+
+            const data =
+            await response.json();
+
+            if (data.secure_url) {
+
+                imageUrl =
+                data.secure_url;
+            }
+        }
+
+
+        /* LƯU FIREBASE */
+
+        await addDoc(
+            collection(db, "posts"),
+            {
+
+                title:
+                title.value,
+
+                content:
+                content.value,
+
+                imageUrl:
+                imageUrl || "",
+
+                userEmail:
+                currentUser.email,
+
+                userId:
+                currentUser.uid,
+
+                createdAt:
+                serverTimestamp()
+            }
+        );
+
+
+        alert("Đăng bài thành công");
+
+
+        title.value = "";
+
+        content.value = "";
+
+        imageInput.value = "";
+
+
+        loadPosts();
+
+    } catch (error) {
+
+        console.log(error);
+
+        alert(error.message);
+    }
+};
     if(title.value.trim() === ""){
 
         alert("Nhập tiêu đề");
